fc26fcdaa37b05f45966f9dac940efdb087d3873
kdando <kvdando@gmail.com>
2024-01-18 14:40:01 +0000

    corrected msitake: now export Pool(config) not Pool()

--- db/connection.js
+++ db/connection.js
@@ -7,6 +7,10 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
+
 const config = {};
 
 if (ENV === "production") {
@@ -14,10 +18,4 @@ if (ENV === "production") {
   config.max = 2;
 }
 
-
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
-}
-
-module.exports = new Pool();
+module.exports = new Pool(config);
